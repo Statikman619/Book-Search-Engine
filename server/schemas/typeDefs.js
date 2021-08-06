29f83547d5d97699e2d88c0db44f3cb642360ec5
Statikman619 <martinez.michael02@gmail.com>
2021-08-05 20:24:51 -0700

    created a resolvers.js and typeDefs.js file

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,29 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type Book {
+    _id: ID!
+    bookId: String
+    authors: [String]
+    # authors: String
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+type User {
+    _id: ID!
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+input savedBook {
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+    authors: [String]
+}
