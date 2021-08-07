e0dacab360ae7ef8e952148447f057a991c69e83
Statikman619 <martinez.michael02@gmail.com>
2021-08-07 09:05:52 -0700

    trying to add a saveBook option

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -49,3 +49,19 @@ const resolvers = {
             return {token, user};
     
         },
+
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+            //   const savedBook = await Book.create({ ...args, username: context.user.username });
+          
+             const updatedUser =  await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: args.input } },
+                { new: true }
+              );
+          
+            return updatedUser;
+            }
+          
+            throw new AuthenticationError('You need to be logged in!');
+        },
