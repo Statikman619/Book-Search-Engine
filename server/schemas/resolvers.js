008505934065223f89a6986b6e8c46a4e9556e21
Statikman619 <martinez.michael02@gmail.com>
2021-08-18 18:28:28 -0700

    fixed my log in issue

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,7 @@ const resolvers = {
     //get a user by username
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({})
+        const userData = await User.findOne({ _id: context.user._id })
           .select("-__v -password")
           .populate("books");
 
